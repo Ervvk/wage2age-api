080d97d34c36ea20b7ccc058696b07b5a1c00cea
Ervk <UL0245422@edu.uni.lodz.pl>
2022-08-28 21:26:00 +0200

    wrap up code

--- routes/offers.js
+++ routes/offers.js
@@ -12,9 +12,7 @@ router.route("/").get((req, res) => {
 });
 
 router.route("/offers").get((req, res) => {
-  console.log("test");
   dboperations.getOffers().then((result) => {
-    res.json("chomik");
     if (result) {
       res.json(result);
     } else {
