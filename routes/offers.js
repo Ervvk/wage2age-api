e337ffc9f3afe4e2eebd0823926a376fc88585bb
Ervk <UL0245422@edu.uni.lodz.pl>
2022-08-28 16:13:27 +0200

    heroku works cd

--- routes/offers.js
+++ routes/offers.js
@@ -12,6 +12,7 @@ router.route("/").get((req, res) => {
 });
 
 router.route("/offers").get((req, res) => {
+  res.json("Offers");
   console.log("test");
   dboperations.getOffers().then((result) => {
     if (result) {
